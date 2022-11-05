94d2137bb047bf99e88de56d6de0c8a3ebfa321c
Anthony Salinas <salinasgo_a@lfmurcie.org>
2022-11-05 01:03:57 +0100

    Solved day 3 second problem part

--- 3/main.py
+++ 3/main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Callable
 
 
 class Problem3Solver:
@@ -26,10 +27,10 @@ class Problem3Solver:
 			for line in input_file:
 				# For each binary number, check bit by bit
 				nb_lines += 1
-				for i, letter in enumerate(line.strip()):
+				for i, digit in enumerate(line.strip()):
 					# Count the number of ones at each
 					# bit position
-					nb_ones[i] += 1 if letter == '1' else 0
+					nb_ones[i] += 1 if digit == '1' else 0
 
 			# Calculate both the gamma and epsilon rate value
 			gamma_rate_str = ''
@@ -46,7 +47,106 @@ class Problem3Solver:
 
 	@staticmethod
 	def get_life_support_rating(file: str) -> int:
-		return 0
+		'''
+		Gets the life support rating by multiplying
+		the oxygen generator rating times the CO2 scrubber
+		rating
+
+		Args:
+			file (str): The input file path
+
+		Returns:
+			int: The product of the oxygen generator rating
+			and the CO2 scrubber rating
+		'''
+		with open(file, 'r', encoding='utf-8') as input_file:
+			all_values = [val.strip() for val in input_file.readlines()]
+
+		# First get the Oxygen generator rating
+		index = 0
+		# While there's more than 1 value
+		values = all_values.copy()
+		nb_values = len(values)
+		while nb_values > 1:
+			# Count the number of '1' at position {bit}
+			nb_ones = 0
+			for val in values:
+				nb_ones += 1 if val[index] == '1' else 0
+
+			if nb_ones >= nb_values / 2 :
+				# Then '1' is the most common bit
+				# Keep the values whose {index} bit is a '1'
+				values = Problem3Solver.__keep_expected_val(
+					'1', values, index)
+			else :
+				# Then '0' is the most common bit
+				# Keep the values whose {index} bit is a '0'
+				values = Problem3Solver.__keep_expected_val(
+					'0', values, index)
+
+			index += 1
+			nb_values = len(values)
+		oxygen_generator_rating_bin = values[0]
+		oxygen_generator_rating = int(
+			oxygen_generator_rating_bin, base=2
+		)
+
+		# Then get the CO2 Scrubber rating
+		index = 0
+		# While there's more than 1 value
+		values = all_values.copy()
+		nb_values = len(values)
+		while nb_values > 1:
+			# Count the number of '1' at position {bit}
+			nb_ones = 0
+			for val in values:
+				nb_ones += 1 if val[index] == '1' else 0
+
+			if nb_ones >= nb_values / 2 :
+				# Then '0' is the least common bit
+				# Keep the values whose {index} bit is a '0'
+				values = Problem3Solver.__keep_expected_val(
+					'0', values, index)
+			else :
+				# Then '1' is the least common bit
+				# Keep the values whose {index} bit is a '0'
+				values = Problem3Solver.__keep_expected_val(
+					'1', values, index)
+
+			index += 1
+			nb_values = len(values)
+		co2_scrubber_rating_bin = values[0]
+		co2_scrubber_rating = int(
+			co2_scrubber_rating_bin, base=2
+		)
+
+		return oxygen_generator_rating * co2_scrubber_rating
+
+	@staticmethod
+	def __keep_expected_val(
+		val: str,
+		values: list[str],
+		pos: int
+	) -> list[str]:
+		'''
+		Takes a list of binary numbers and keeps only
+		the elements whose {pos}th bit is equal to the
+		given expected value
+
+		Args:
+			val (str): The given expected value
+			values (list[str]): The list of binary numbers
+			pos (int): The bit index to check
+
+		Returns:
+			list[str]: The list of values that fit the
+			condition
+		'''
+		is_expected_val: 'Callable[[str], bool]'= (
+			lambda x: x[pos] == val
+		)
+		kept_values = list(filter(is_expected_val, values))
+		return kept_values
 
 def main() -> None:
 	'''
